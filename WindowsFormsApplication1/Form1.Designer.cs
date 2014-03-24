d2602a0836f4db52dab31850849f74042f78e86d
kaskanoidas <rolandas.rimkus.fw@gmail.com>
2014-03-24 15:12:58 +0200

    Be ciklo

---
+++ WindowsFormsApplication1/Form1.Designer.cs
@@ -0,0 +1,181 @@
+﻿namespace WindowsFormsApplication1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.richTextBox2 = new System.Windows.Forms.RichTextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.comboBox3 = new System.Windows.Forms.ComboBox();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(505, 19);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(89, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Start counting";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(10, 54);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(61, 13);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Uzsakymas";
+            // 
+            // richTextBox2
+            // 
+            this.richTextBox2.Location = new System.Drawing.Point(233, 70);
+            this.richTextBox2.Name = "richTextBox2";
+            this.richTextBox2.Size = new System.Drawing.Size(354, 221);
+            this.richTextBox2.TabIndex = 3;
+            this.richTextBox2.Text = "";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(230, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(57, 13);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Rezultatas";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.DropDownWidth = 70;
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(89, 21);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(82, 21);
+            this.comboBox2.TabIndex = 7;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 24);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(71, 13);
+            this.label4.TabIndex = 8;
+            this.label4.Text = "Medzio rusys:";
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(12, 70);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(215, 221);
+            this.richTextBox1.TabIndex = 9;
+            this.richTextBox1.Text = "";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(192, 24);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(74, 13);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "Juosteles ilgis:";
+            // 
+            // comboBox3
+            // 
+            this.comboBox3.DropDownWidth = 70;
+            this.comboBox3.FormattingEnabled = true;
+            this.comboBox3.Location = new System.Drawing.Point(272, 21);
+            this.comboBox3.Name = "comboBox3";
+            this.comboBox3.Size = new System.Drawing.Size(82, 21);
+            this.comboBox3.TabIndex = 11;
+            this.comboBox3.SelectedIndexChanged += new System.EventHandler(this.comboBox3_SelectedIndexChanged);
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(373, 24);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(38, 13);
+            this.label6.TabIndex = 12;
+            this.label6.Text = "Kiekis:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(417, 21);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(82, 20);
+            this.textBox1.TabIndex = 13;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(599, 303);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.comboBox3);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.richTextBox2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Sablonu uzpildymas";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RichTextBox richTextBox2;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.ComboBox comboBox3;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WindowsFormsApplication1/Form1.cs
@@ -0,0 +1,423 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Text;
+using System.Windows.Forms;
+
+namespace WindowsFormsApplication1
+{
+    public partial class Form1 : Form
+    {
+        Rusys duom = new Rusys();
+        Rusis duomenys = new Rusis();
+        Sablonai sabl = new Sablonai();
+        Elementas elem = new Elementas();
+        Uzklausa problem = new Uzklausa();
+        Uzklausa visoP = new Uzklausa();
+        RandomiserClass Randomiser = new RandomiserClass();
+        RandomElements RandomList = new RandomElements();
+        int paskutinisNr = -1;
+        int viso = 0;
+        public Form1()
+        {
+            InitializeComponent();
+            GetRusisDuomenys();
+            GetSablonuDuomenys();
+            GetJuosteliuIlgiai();
+        }
+        private void GetRusisDuomenys()
+        {
+            System.IO.StreamReader file = new System.IO.StreamReader("C:\\Users\\Rolandas\\Desktop\\Apps\\WindowsFormsApplication1\\WindowsFormsApplication1\\Rusys.txt");
+            while (file.EndOfStream != true)
+            {
+                int galas = 0;
+                string vardas = file.ReadLine();
+                int tarpas = vardas.IndexOf(",", galas);
+                duom.vardas.Add(vardas.Substring(0, tarpas));
+                int j = 0;
+                Boolean breakWhile = false;
+                duomenys = new Rusis();
+                while (breakWhile == false)
+                {
+                    string MedzioRusis = "";
+                    double Pradzia = -1;
+                    double Pabaiga = -1;
+                    galas = vardas.IndexOf(":", tarpas);
+                    MedzioRusis = vardas.Substring(tarpas + 2, galas - tarpas - 2);
+                    tarpas = galas; galas = vardas.IndexOf("-", tarpas);
+                    if (galas < 0)
+                    {
+                        galas = vardas.IndexOf(",", tarpas);
+                        if (galas < 0)
+                        {
+                            galas = vardas.IndexOf("!", tarpas);
+                            breakWhile = true;
+                        }
+                        Pradzia = Pabaiga = Convert.ToDouble(vardas.Substring(tarpas + 1, galas - tarpas - 1));
+                        tarpas = galas;
+                    }
+                    else
+                    {
+                        Pradzia = Convert.ToDouble(vardas.Substring(tarpas + 1, galas - tarpas - 1));
+                        tarpas = galas;
+                        galas = vardas.IndexOf(",", tarpas);
+                        if (galas < 0)
+                        {
+                            galas = vardas.IndexOf("!", tarpas);
+                            breakWhile = true;
+                        }
+                        Pabaiga = Convert.ToDouble(vardas.Substring(tarpas + 1, galas - tarpas - 1));
+                        tarpas = galas;
+                    }
+                    duomenys.pav.Add(MedzioRusis);
+                    duomenys.pradzia.Add(Pradzia);
+                    duomenys.pabaiga.Add(Pabaiga);
+                    j++;
+                }
+                duom.Rus.Add(duomenys);
+            }
+        }
+        private void GetSablonuDuomenys()
+        {
+            System.IO.StreamReader file = new System.IO.StreamReader("C:\\Users\\Rolandas\\Desktop\\Apps\\WindowsFormsApplication1\\WindowsFormsApplication1\\Sablonai.txt");
+            while (file.EndOfStream != true)
+            {
+                elem = new Elementas();
+                string[] vardas = file.ReadLine().Split();
+                sabl.SablonoNr.Add(int.Parse(vardas[0]));
+                int i = 1;
+                while (i < vardas.Length)
+                {
+                    elem.JuostIlgis.Add(int.Parse(vardas[i++]));
+                    elem.Kiekis.Add(int.Parse(vardas[i++]));
+                }
+                sabl.SablonoElem.Add(elem);
+            }
+        }
+        private void GetJuosteliuIlgiai()
+        {
+            comboBox3.Items.Add(15);
+            comboBox3.Items.Add(20);
+            comboBox3.Items.Add(25);
+            comboBox3.Items.Add(30);
+            comboBox3.Items.Add(35);
+            comboBox3.Items.Add(40);
+            comboBox3.Items.Add(45);
+            comboBox3.Items.Add(48);
+            comboBox3.Items.Add(50);
+            comboBox3.Items.Add(60);
+            for (int i = 0; i < duom.Rus.Count; i++)
+            {
+                for (int j = 0; j < duom.Rus[i].pav.Count; j++)
+                {
+                    string test = duom.Rus[i].pav[j];
+                    if (comboBox2.Items.IndexOf(test) < 0)
+                    {
+                        comboBox2.Items.Add(test);
+                    }
+                }
+            }
+        }
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Reset();
+            richTextBox2.Text = "Uzsakymas:" + "\n" + richTextBox1.Text + "\n" + "\n";
+            richTextBox2.Text += "Rezultatas:" + "\n";
+            ReadTextBox();
+            AtrinktiSchemas();
+            StartEvo();
+        }
+        private void Reset()
+        {
+            problem = new Uzklausa();
+            visoP = new Uzklausa();
+            Randomiser = new RandomiserClass();
+            RandomList = new RandomElements();
+            paskutinisNr = -1;
+            viso = 0;
+        }
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (richTextBox1.Text == "")
+            {
+                richTextBox1.Text += comboBox2.SelectedItem + " ";
+            }
+            else
+            {
+                richTextBox1.Text += "\n" + comboBox2.SelectedItem + " ";
+            }
+            paskutinisNr = 1;
+        }
+        private void comboBox3_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            if (paskutinisNr == 1)
+            {
+                richTextBox1.Text +=  comboBox3.SelectedItem + " ";
+            }
+            else
+            {
+                richTextBox1.Text += "\n" + comboBox2.SelectedItem + " " + comboBox3.SelectedItem + " ";
+            }
+            paskutinisNr = 2;
+        }
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyValue == 13)
+            {
+                richTextBox1.Text += textBox1.Text;
+                paskutinisNr = 3;
+            }
+        }
+        private int ReadTextBox()
+        {
+            string[] text = richTextBox1.Text.Split('\n');
+            for (int i = 0; i < text.Length; i++)
+            {
+                string[] eilute = text[i].Split();
+                problem.tipai.Add(eilute[0]);
+                problem.ilgis.Add(int.Parse(eilute[1]));
+                problem.kiekis.Add(int.Parse(eilute[2]));
+            }
+            int NR = RastiTinkamiausiaRusi();
+            richTextBox2.Text += "Dizaino numeris: "+ NR + "\n";
+            return NR;
+        }
+        private int RastiTinkamiausiaRusi()
+        {
+            List<string> AtrinktiTipai = new List<string> { };
+            List<int> AtrinktuSumos = new List<int> { };
+            for (int i = 0; i < problem.ilgis.Count; i++)
+            {
+                if (AtrinktiTipai.IndexOf(problem.tipai[i]) < 0)
+                {
+                    AtrinktiTipai.Add(problem.tipai[i]);
+                    AtrinktuSumos.Add(problem.kiekis[i]);
+                }
+                else
+                {
+                    AtrinktuSumos[AtrinktiTipai.IndexOf(problem.tipai[i])] += problem.kiekis[i];
+                }
+                if (visoP.ilgis.IndexOf(problem.ilgis[i]) < 0)
+                {
+                    visoP.ilgis.Add(problem.ilgis[i]);
+                    visoP.kiekis.Add(problem.kiekis[i]);
+                }
+                else
+                {
+                    visoP.kiekis[visoP.ilgis.IndexOf(problem.ilgis[i])] += problem.kiekis[i];
+                }
+            }
+            int suma = 0;
+            for (int i = 0; i < AtrinktuSumos.Count; i++)
+            {
+                suma += AtrinktuSumos[i];
+            }
+            viso = suma;
+            double santykis = Convert.ToDouble(AtrinktuSumos[0]) / suma;
+            double ApvalintasSantykis = Math.Round(santykis * 100, 0);
+            problem.santykis = ApvalintasSantykis;
+            visoP.santykis = ApvalintasSantykis;
+            List<Rusis> RusisAtrinkimui = new List<Rusis>{};
+            double min = 99999; int mn = -1;
+            for (int i = 0; i < duom.Rus.Count; i++)
+            {
+                if (duom.Rus[i].pav.Count == AtrinktiTipai.Count)
+                {
+                    int count = 0;
+                    for (int j = 0; j < duom.Rus[i].pav.Count; j++)
+                    {
+                        
+                        if (AtrinktiTipai.IndexOf(duom.Rus[i].pav[j]) >= 0)
+                        {
+                            count++;
+                        }
+                    }
+                    if (count == AtrinktiTipai.Count)
+                    {
+                        double pradzia = duom.Rus[i].pradzia[duom.Rus[i].pav.IndexOf(AtrinktiTipai[0])];
+                        double pabaiga = duom.Rus[i].pabaiga[duom.Rus[i].pav.IndexOf(AtrinktiTipai[0])];
+                        if (pradzia <= ApvalintasSantykis && pabaiga >= ApvalintasSantykis)
+                        {
+                            richTextBox2.Text += "Optimaliausias dizainas:  " + duom.vardas[i] + "\n";
+                            return i;
+                        }
+                        else
+                        {
+                            if (Math.Min(pradzia - ApvalintasSantykis, ApvalintasSantykis - pabaiga) < min)
+                            {
+                                min = Math.Min(pradzia - ApvalintasSantykis, ApvalintasSantykis - pabaiga);
+                                mn = i;
+                            }
+                        }
+                    }
+                }
+            }
+            if (mn != -1)
+            {
+                richTextBox2.Text += "Optimaliausias dizainas:  " + duom.vardas[mn] + "\n";
+            }
+            return mn;
+        }
+        private void AtrinktiSchemas()
+        {
+            richTextBox2.Text += "Schemos atitinkancios uzklausos parametrus:" + "\n";
+            Sablonai sablonaiBeta = new Sablonai();
+            for (int i = 0; i < sabl.SablonoNr.Count; i++)
+            {
+                int count = 0;
+                for(int j = 0; j < visoP.ilgis.Count; j++)
+                {
+                    if (sabl.SablonoElem[i].JuostIlgis.IndexOf(visoP.ilgis[j]) >= 0)
+                    {
+                        count++;
+                    }
+                }
+                if (count == sabl.SablonoElem[i].JuostIlgis.Count)
+                {
+                    sablonaiBeta.SablonoNr.Add(sabl.SablonoNr[i]);
+                    sablonaiBeta.SablonoElem.Add(sabl.SablonoElem[i]);
+                    richTextBox2.Text += sabl.SablonoNr[i] + " ";
+                }
+            }
+            richTextBox2.Text += "\n";
+            sabl = sablonaiBeta;
+        }
+        private void StartEvo()
+        {
+            richTextBox2.Text += "\n" + "Galutinis rezultatas:" + "\n";
+            Randomiser.sablonas = sabl;
+            NykstukuFabrikas(500);
+            Testing();
+        }
+        private void NykstukuFabrikas(int kiekis)
+        {
+            Random r = new Random();
+            List<int> minimumai = new List<int> { };
+            richTextBox2.Text +="Viso juosteliu: " + viso + " " + "Rastu sablonu skaicius: " +  Randomiser.sablonas.SablonoNr.Count + "\n";
+            for (int i = 0; i < Randomiser.sablonas.SablonoNr.Count; i++)
+            {
+                int min = 99999;
+                for (int j = 0; j < Randomiser.sablonas.SablonoElem[i].JuostIlgis.Count; j++)
+                {
+                    int index = visoP.ilgis.IndexOf(Randomiser.sablonas.SablonoElem[i].JuostIlgis[j]);
+                    if (index != -1)
+                    {
+                        int dalyba = Convert.ToInt32(Math.Floor(Convert.ToDouble(visoP.kiekis[index]) / Convert.ToDouble(Randomiser.sablonas.SablonoElem[i].Kiekis[j])));
+                        if (dalyba < min)
+                        {
+                            min = dalyba;
+                        }
+                    }
+                }
+                minimumai.Add(min);
+                richTextBox2.Text += "Schemos " + Randomiser.sablonas.SablonoNr[i] + " minimumas: " + min + "\n";
+            }
+            for (int kiek = 0; kiek < kiekis; kiek++) // keisti (100)
+            {
+                Randomiser = new RandomiserClass();
+                Randomiser.sablonas = sabl;
+                for (int add = 0; add < Randomiser.sablonas.SablonoNr.Count; add++)
+                {
+                    Randomiser.kiekis.Add(r.Next(0, minimumai[add]));
+                }
+                RandomList.random.Add(Randomiser);
+            }
+        }
+        private void Testing()
+        {
+            RandomElements rand = new RandomElements();
+            richTextBox2.Text += "Atimimas: " + "\n";
+            for (int i = 0; i < RandomList.random.Count; i++)
+            {
+                Boolean deti = true;
+                List<int> viso = new List<int> { };
+                for (int get = 0; get < visoP.kiekis.Count; get++)
+                {
+                    viso.Add(visoP.kiekis[get]);
+                }
+                for (int j = 0; j < RandomList.random[i].sablonas.SablonoNr.Count; j++)
+                {
+                    for(int h = 0; h < RandomList.random[i].sablonas.SablonoElem[j].Kiekis.Count; h++)
+                    {
+                        int atimti = RandomList.random[i].sablonas.SablonoElem[j].Kiekis[h] * RandomList.random[i].kiekis[j];
+                        int index = visoP.ilgis.IndexOf(RandomList.random[i].sablonas.SablonoElem[j].JuostIlgis[h]);
+                        viso[index] -= atimti;
+                    }
+                }
+                int suma = 0;
+                for (int sum = 0; sum < viso.Count; sum++)
+                {
+                    RandomList.random[i].sumos.Add(viso[sum]);
+                    if (viso[sum] < 0)
+                    {
+                        deti = false;
+                    }
+                    suma += Math.Abs(viso[sum]);
+                }
+                RandomList.random[i].liekana = suma;
+                if (deti == true)
+                {
+                    rand.random.Add(RandomList.random[i]);
+                }
+            }
+            RandomList = rand;
+            RandomList.SortList();
+            for (int i = 0; i < 5; i++)
+            {
+                for (int h = 0; h < visoP.kiekis.Count; h++)
+                {
+                    richTextBox2.Text += "Ilgis: " + visoP.ilgis[h] + " " + RandomList.random[i].sumos[h] + "\n";
+                }
+                richTextBox2.Text += "Liekana: " + RandomList.random[i].liekana + "\n" + "\n";
+            }
+        }
+    }
+}
+public class Rusys
+{
+    public List<string> vardas = new List<string>{};
+    public List<Rusis> Rus = new List<Rusis>{};
+}
+public class Rusis
+{
+    public List<string> pav = new List<string>{};
+    public List<double> pradzia = new List<double> { };
+    public List<double> pabaiga = new List<double> { };
+}
+public class Sablonai
+{
+    public List<int> SablonoNr = new List<int> { };
+    public List<Elementas> SablonoElem = new List<Elementas> { };
+}
+public class Elementas
+{
+    public List<int> JuostIlgis = new List<int> { };
+    public List<int> Kiekis = new List<int> { };
+}
+public class Uzklausa
+{
+    public double santykis;
+    public List<string> tipai = new List<string>{};
+    public List<int> kiekis = new List<int> { };
+    public List<int> ilgis = new List<int> { };
+}
+public class RandomiserClass
+{
+    public Sablonai sablonas = new Sablonai();
+    public List<int> kiekis = new List<int> { };
+    public int liekana;
+    public List<int> sumos = new List<int> { };
+}
+public class RandomElements
+{
+    public List<RandomiserClass> random = new List<RandomiserClass> { };
+    public void SortList()
+    {
+        random.Sort(delegate(RandomiserClass a, RandomiserClass b)
+        {
+            return a.liekana.CompareTo(b.liekana);
+        });
+    }
+}
